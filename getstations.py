49dc6ea1889b4510f6d25d3d59a151b4363b5643
Ben Everson <ben.everson@gmail.com>
2014-08-25 11:58:09 -0400

    Final Commit before initial deploy.

--- getstations.py
+++ getstations.py
@@ -9,18 +9,29 @@ import re
 import csv
 
 STATIONS_ENDPOINT = 'http://citibikenyc.com/stations/json'
-DATAFILES_PATH = 'datafiles/'
+DATAFILES_PATH = 'datafiles/archive/datafiles/'
 
-def get_recent_statuses(num=20):
+def get_recent_statuses(num=100):
     # files are stored in the datafiles directory
     filenames = listdir(DATAFILES_PATH) # get a list of all file names in directory
-    _mostrecent = []
-    for _fname in reversed(filenames): # iterate over all file names, most to least recent
+    _filedates = []
+    for _fname in filenames: # iterate over all file names, most to least recent
         # check if the file name conforms to the proper convention
-        m = re.match('([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2})_([0-9]{2})_([0-9]{2}).csv', _fname)
-        if m and len(_mostrecent) < num: _mostrecent.append(_fname) 
+        m = re.match('([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2})_([0-9]{2})_([0-9]{2})', _fname)
+        if m and len(_filedates) < num: _filedates.append(strptime(m.group(0), '%Y-%m-%d %H_%M_%S')) 
     
-    # _mostrecent now holds the 'num' most recent file names
+    # _mostrecent now holds the 'num' most recent file dates
+    _filedates.sort() # sort the list
+    _filedates.reverse() # make most recent dates first
+    
+    # a list to hold the assosciated file names
+    _mostrecent = []
+    
+    for _fd in _filedates:
+        # generate the name
+        _filename = strftime('%Y-%m-%d %H_%M_%S', _fd) + '.csv'
+        #print 'Archiving ' + _filename
+        _mostrecent.append(_filename)
     
     # create a dict to hold all of the 'num' most recent data files
     # key will be the timestamp, and value will be the station statuses at that time
